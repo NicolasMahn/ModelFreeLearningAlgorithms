fca858c3099b8f4c2950cef53799b6e2d50c8a23
nicolas <nicolas.mahn@gmx.de>
2022-12-27 23:25:57 +0100

    final commit

--- main.py
+++ main.py
@@ -7,78 +7,79 @@ import math
 def main():
     cliff = env.Cliff()
     labyrinth = env.Labyrinth()
-    ttt = env.TicTacToe(1)
+    ttt_1 = env.TicTacToe(1)
+    ttt_2 = env.TicTacToe(2)
 
-    # sh.execute_monte_carlo(labyrinth)
-    # sh.avg_monte_carlo(labyrinth, 20)
+    print("What would Information would you like?")
+    print("1.  execute Monte Carlo with the labyrinth model (as in figure 18)")
+    print("2.  average execution of Monte Carlo with the labyrinth model (as in figure 19)")
+    print("3.  average execution of TD(0) compared to MC const alpha in the labyrinth model (as in figure 20)")
+    print("4.  execute TD(0) with the labyrinth model (as in figure 21)")
+    print("5.  average execution of different n-step TD algorithms in the labyrinth model (as in figure 23)")
+    print("6.  execute SARSA with the labyrinth model (as in figure 26)")
+    print("7.  average execution of SARSA compared to TD(0) in the labyrinth model (as in figure 27)")
+    print("8.  average execution of Q-Learning compared to SARSA in the labyrinth model (as in figure 30)")
+    print("9.  average execution of Q-Learning compared to SARSA in the cliff walking example with no epsilon decay "
+          "(as in figure 32)")
+    print("10. average execution of Q-Learning compared to SARSA in the cliff walking example with epsilon decay "
+          "(as in figure 34)")
+    print("11. average execution of Q-Learning with the TicTacToe model as player 1 (as in figure 35)")
+    print("12. average execution of Q-Learning with the TicTacToe model as player 1 showing the win rate"
+          " (as in figure 36)")
+    print("13. average execution of Q-Learning with the TicTacToe model as player 2 showing the win rate"
+          " (as in figure 37)")
+    print("13. average execution of Q-Learning vs Q-Learning with the TicTacToeVS model showing the win rate"
+          " (as in figure 37)")
 
-    # sh.execute_td_0(labyrinth)
-    # sh.avg_td_0(labyrinth, 1000)
-    # sh.vs2(labyrinth, 500, 1000, alg.td_0, "TD(0)", alg.monte_carlo_alpha, "MC")
+    num = input("please input the number of the wanted result \n")
+    print("\n")
 
-    # sh.vs_multi_with_par(labyrinth, 1000, [alg.td_0, alg.td_n, alg.td_n, alg.td_n, alg.td_n, alg.td_n,
-    #                                    alg.monte_carlo_alpha],
-    #                     ["1-step TD", "2-step TD", "4-step TD", "6-step TD", "8-step TD", "16-step TD", "MC"],
-    #                     "Comparing TD Algorithms", n_list=[None, 2, 4, 6, 8, 16, None])
-
-    # sh.vs_multi_with_par(ttt, 100, [alg.q_learning, alg.q_learning, alg.q_learning],
-    #                     ["alpha = 0.3", "alpha = 0.1", "alpha = 0.01"],
-    #                     "Comparing epsilons for Q-Learning",
-    #                     episodes_list=1000,
-    #                     epsilon_list=0, epsilon_decay_list=1,
-    #                     alpha_list=[0.3, 0.1, 0.01])
-
-    # sh.avg_q_learning_tictactoe(episodes=15000, smart=True)
-
-    #sh.vs_multi_with_par(cliff, 1000, [alg.sarsa, alg.q_learning],
-    #                     ["SARSA", "Q-Learning"], "Cliff Walking",
-    #                     prev_state_list=True,
-    #                     episodes_list=1000,
-    #                     epsilon_list=0.2, epsilon_decay_list=1)
-
-    sh.vs_multi_with_par(cliff, 1000, [alg.sarsa, alg.q_learning],
-                         ["SARSA", "Q-Learning"], "Cliff Walking",
-                         prev_state_list=True,
-                         episodes_list=1000,
-                         epsilon_list=0.2)
-
-    # vs_multi(cliff, 100, [quality_algorithm.sarsa, quality_algorithm.q_learning],
-    #          ["SARSA", "Q-Learning"],
-    #          [None, None], "Cliff Walking")
-
-    # vs_multi(labyrinth, 1000, [monte_carlo_algorithm.monte_carlo, monte_carlo_algorithm.monte_carlo_constant_alpha],
-    #         ["classical MC", "MC const alpha 0.5"],
-    #         [None, 2, 4, 8, 16, 32, None], "Comparing MC Algorithms" episodes_list, gamma_list, epsilon_list, alpha_list, epsilon_decay_list)
-
-    # vs_multi(labyrinth, 1000, [value_algorithm.td_0, monte_carlo_algorithm.monte_carlo], ["TD(0)", "MC"], "TD(0) compared to MC")
-
-    # vs_multi_with_par(labyrinth, 1000, [quality_algorithm.q_learning, quality_algorithm.sarsa], n_list=[None,None], episodes_list=[1000,1000], gamma_list=[None,None], epsilon_list=[None,None], alpha_list=[0.1, 0.1], epsilon_decay_list=[None,None,], function_names=["Q-Learning", "SARSA"], title="Q-Learning compared to SARSA")
-
-    # vs_multi_with_par(labyrinth, 1000, [quality_algorithm.sarsa, value_algorithm.td_0], n_list=[None,None], episodes_list=[None,None], gamma_list=[None,None], epsilon_list=[None,None], alpha_list=[0.1, 0.1], epsilon_decay_list=[None,None,], function_names=["SARSA", "TD(0)"], title="SARSA compared to TD(0)")
-
-    # vs_multi_with_par(labyrinth, 100, functions=[quality_algorithm.sarsa, quality_algorithm.sarsa, quality_algorithm.sarsa, value_algorithm.td_0, value_algorithm.td_n, quality_algorithm.q_learning], n_list=[None,None,None,None,2,None], episodes_list=[None,None,None,None,None,None], gamma_list=[None,None,None,None,None,None], epsilon_list=[None,None,None,None,None,None], alpha_list=[0.5, 0.1, 0.01, 0.01, 0.01, 0.01], epsilon_decay_list=[None,None,None, None, None,None], function_names=["alpha 0.5", "alpha 0.1", "alpha 0.01", "TD(0)", "2-step TD", "Q"], title="SARSA alpha test")
-
-    # sh.vs2(labyrinth, 100, 500, value_algorithm.td_0, "TD(0)", monte_carlo_algorithm.monte_carlo, "MC")
-    # execute_td_0(labyrinth)
-    # execute_q_learning(labyrinth)
-    # avg_q_learning(labyrinth, 1000)
-    # execute_monte_carlo(labyrinth)
-    # avg_monte_carlo(labyrinth, 1000)
-    # avg_td_0(labyrinth, 100)
-
-    # execute_q_learning(cliff)
-    # execute_sarsa(cliff)
-    # avg_sarsa(cliff, 100)
-    # avg_q_learning(tictactoe, 100)
-
-    # execute_q_learning(labyrinth)
-
-    # execute_q_learning(tictactoe_smart)
-    # sh.avg_q_learning(tictactoe, 1000, episodes=1000)
-
-    # avg_q_learning_tictactoe(number_of_iterations=1000, episodes=10000, smart=True, title="TicTacToe | Q-Learning vs Q-Learning")
-
-    # avg_q_learning_tictactoe(player=2, number_of_iterations=1000, episodes=1000, smart=False, title="TicTacToe | Random vs Q-Learning")
+    if num == "1":
+        sh.execute_monte_carlo(labyrinth)
+    elif num == "2":
+        sh.avg_monte_carlo(labyrinth, 1000)
+    elif num == "3":
+        sh.vs2(labyrinth, 500, 1000, alg.td_0, "TD(0)", alg.monte_carlo_alpha, "MC")
+    elif num == "4":
+        sh.execute_td_0(labyrinth)
+    elif num == "5":
+        print("This takes over 1h of calculation time")
+        sh.vs_multi_with_par(labyrinth, 1000, [alg.td_0, alg.td_n, alg.td_n, alg.td_n, alg.td_n, alg.td_n,
+                                               alg.monte_carlo_alpha],
+                                ["1-step TD", "2-step TD", "4-step TD", "6-step TD", "8-step TD", "16-step TD", "MC"],
+                                "Comparing TD Algorithms", n_list=[None, 2, 4, 6, 8, 16, None])
+    elif num == "6":
+        sh.execute_sarsa(labyrinth)
+    elif num == "7":
+        sh.vs_multi_with_par(labyrinth, 1000, [alg.sarsa, alg.td_0], ["SARSA", "TD(0)"], "SARSA compared to TD(0)",
+                             alpha_list=0.1)
+    elif num == "8":
+        sh.vs_multi_with_par(labyrinth, 1000, [alg.q_learning, alg.sarsa], ["Q-Learning", "SARSA"],
+                             "Q-Learning compared to SARSA",
+                             alpha_list=0.1)
+    elif num == "9":
+        sh.vs_multi_with_par(cliff, 1000, [alg.sarsa, alg.q_learning],
+                             ["SARSA", "Q-Learning"], "Cliff Walking",
+                             prev_state_list=True,
+                             episodes_list=1000,
+                             epsilon_list=0.2, epsilon_decay_list=1)
+    elif num == "10":
+        sh.vs_multi_with_par(cliff, 1000, [alg.sarsa, alg.q_learning],
+                             ["SARSA", "Q-Learning"], "Cliff Walking",
+                             prev_state_list=True,
+                             episodes_list=1000,
+                             epsilon_list=0.2)
+    elif num == "11":
+        sh.avg_q_learning(ttt_1, 1000, 1000)
+    elif num == "12":
+        print("This takes some calculation time")
+        sh.avg_q_learning_tictactoe(episodes=1000, player=1, smart=False)
+    elif num == "13":
+        print("This takes some calculation time")
+        sh.avg_q_learning_tictactoe(episodes=1500, player=2, smart=False)
+    elif num == "14":
+        print("This takes over 3h of calculation time")
+        sh.avg_q_learning_tictactoe(episodes=15000, smart=True)
 
 
 if __name__ == '__main__':
